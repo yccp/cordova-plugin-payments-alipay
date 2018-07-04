e7fd0dda49a12ecc96d0c1dd0fdc5679472dbd99
Yu Chen <yu.chen@live.ie>
2018-07-05 04:01:14 +0800

    添加授权

--- src/ios/Alipay.h
+++ src/ios/Alipay.h
@@ -4,8 +4,10 @@
 @interface Alipay : CDVPlugin
 
 @property(nonatomic,strong)NSString *appId;
+@property(nonatomic,strong)NSString *schema;
 @property(nonatomic,strong)NSString *currentCallbackId;
 
 - (void)pay:(CDVInvokedUrlCommand*)command;
+- (void) auth:(CDVInvokedUrlCommand*)command;
 
 @end
